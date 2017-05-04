8dd9fc78f5b57a186ac8cabb97fc8e52ee5cd539
signeus <coolasduck09@gmail.com>
2017-05-05 01:37:28 +0200

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SweetSweetGoodnight
+{
+    partial class SweetSweetGoodnight
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.connect = new System.Windows.Forms.Button();
+            this.abort = new System.Windows.Forms.Button();
+            this.temporizador = new System.Windows.Forms.Timer(this.components);
+            this.lblTemporizador = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbIp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.connect.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect.Location = new System.Drawing.Point(65, 12);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(89, 23);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "Conectar";
+            this.connect.UseVisualStyleBackColor = false;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // abort
+            // 
+            this.abort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.abort.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.abort.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.abort.Location = new System.Drawing.Point(12, 63);
+            this.abort.Name = "abort";
+            this.abort.Size = new System.Drawing.Size(268, 43);
+            this.abort.TabIndex = 1;
+            this.abort.Text = "Cancelar el apagado";
+            this.abort.UseVisualStyleBackColor = false;
+            this.abort.Click += new System.EventHandler(this.abort_Click);
+            // 
+            // temporizador
+            // 
+            this.temporizador.Enabled = true;
+            this.temporizador.Interval = 1000;
+            this.temporizador.Tick += new System.EventHandler(this.temporizador_Tick);
+            // 
+            // lblTemporizador
+            // 
+            this.lblTemporizador.AutoSize = true;
+            this.lblTemporizador.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemporizador.Location = new System.Drawing.Point(57, 47);
+            this.lblTemporizador.Name = "lblTemporizador";
+            this.lblTemporizador.Size = new System.Drawing.Size(97, 13);
+            this.lblTemporizador.TabIndex = 2;
+            this.lblTemporizador.Text = "Temporizador: 0";
+            this.lblTemporizador.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Client";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Windows";
+            // 
+            // txbIp
+            // 
+            this.txbIp.AllowDrop = true;
+            this.txbIp.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbIp.HideSelection = false;
+            this.txbIp.Location = new System.Drawing.Point(162, 12);
+            this.txbIp.Name = "txbIp";
+            this.txbIp.Size = new System.Drawing.Size(118, 23);
+            this.txbIp.TabIndex = 5;
+            this.txbIp.Text = "127.0.0.1:9000";
+            // 
+            // SweetSweetGoodnight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(292, 118);
+            this.Controls.Add(this.txbIp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTemporizador);
+            this.Controls.Add(this.abort);
+            this.Controls.Add(this.connect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SweetSweetGoodnight";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Sweet Goodnight";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button abort;
+        private System.Windows.Forms.Timer temporizador;
+        private System.Windows.Forms.Label lblTemporizador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbIp;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using WebSocket4Net;
+using System.Drawing;
+
+namespace SweetSweetGoodnight
+{
+    public partial class SweetSweetGoodnight : Form
+    {
+        private int standartTime = 60;
+        private int time = 0;
+        private Boolean temporizadorActivo = false;
+
+        public SweetSweetGoodnight()
+        {
+            InitializeComponent();
+        }
+        
+        static WebSocket Client;
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            if (connect.Text.Equals("Desconectar"))
+            {
+                Client.Close();
+                connect.Text = "Conectar";
+                this.connect.BackColor = Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            }
+            else if (connect.Text.Equals("Conectar"))
+            {
+                var url = txbIp.Text;
+                Client = new WebSocket("ws://" + url);
+                Client.Opened += new EventHandler(Client_Opened);
+                Client.Error += new EventHandler<SuperSocket.ClientEngine.ErrorEventArgs>(Client_Error);
+                Client.Closed += new EventHandler(Client_Closed);
+                Client.MessageReceived += new EventHandler<MessageReceivedEventArgs>(Client_MessageReceived);
+
+                Client.Open();
+                connect.Text = "Desconectar";
+                connect.BackColor = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            }
+        }
+        
+        void Client_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            Console.WriteLine("Message received : " + e.Message);
+            if(e.Message.Equals("Goodnight"))
+            {
+                temporizadorActivo = true;
+                Process.Start("shutdown", "/s /f");
+            }
+        }
+
+        void Client_Closed(object sender, EventArgs e)
+        {
+            Console.WriteLine("Websocket closed");
+        }
+
+        void Client_Error(object sender, SuperSocket.ClientEngine.ErrorEventArgs e)
+        {
+            Console.WriteLine("Error thrown trying to open websocket : " + e.Exception.Message);
+        }
+
+        void Client_Opened(object sender, EventArgs e)
+        {
+            Console.WriteLine("Websocket connection open.");
+            Client.Send("Hi!! (Windows Version)");
+        }
+
+        private void abort_Click(object sender, EventArgs e)
+        {
+            temporizadorActivo = false;
+            time = 0;
+            lblTemporizador.Text = "Temporizador: 0";
+            Process.Start("shutdown", "/a");
+        }
+
+        private void temporizador_Tick(object sender, EventArgs e)
+        {
+            if (temporizadorActivo)
+            {
+                time++;
+                lblTemporizador.Text = "Temporizador: " + (standartTime-time);
+            }
+        }
+    }
+}
